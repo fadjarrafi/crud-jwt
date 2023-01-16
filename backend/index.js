3d5b94288473941947c99685671020b5616a6068
Fadjar Irfan rafi <dark.fir21@gmail.com>
2023-01-16 21:20:02 +0700

    add logout

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import db from "./config/db.js";
+import cors from "cors";
 import router from './route/index.js';
+import cookieParser from "cookie-parser";
 dotenv.config();
 const app =express();
 
@@ -13,6 +15,8 @@ try {
     console.error(error)
 }
 
+app.use(cors({ credentials:true, origin:'http://localhost:5000'}));
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
