fb989dc1c7a9f36e54606047fdcf7da12c0d25b3
fadjarrafi <dark.fir21@gmail.com>
2023-01-22 03:53:46 +0700

    add register page

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,9 @@ try {
     console.error(error)
 }
 
-app.use(cors({ credentials:true, origin:'http://localhost:5000'}));
+app.use(cors({ credentials:true, origin:'http://localhost:3000'}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, ()=> console.log('Server running at port 5000'))
+app.listen(5000, ()=> console.log('Server running at port 3000'))
